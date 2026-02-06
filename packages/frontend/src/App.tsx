a002e22046431fe0c4596437faeb26af81ab1f2a
byeonggubaek <oranjes@naver.com>
2026-02-06 14:24:37 +0900

    shardcn 설치후

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { greet, type ApiResponse } from 'shared'
+import { Button } from "@/components/ui/button"
 
 function App() {
   const [data, setData] = useState<ApiResponse<string> | null>(null)
@@ -11,7 +12,10 @@ function App() {
   }, [])
 
   return (
-    <div className="p-8 max-w-4xl mx-auto">
+    <div className="gap-4 p-8 max-w-4xl mx-auto">
+      <div className="flex justify-center">
+        <Button className="!text-black !bg-gray-200 hover:!bg-blue-600 hover:!text-white" variant="default">Shard Button</Button> 
+      </div>
       <h1 className="text-3xl font-bold mb-8">{greet('ESM Frontend')}</h1>
       <pre className="bg-gray-100 p-4 rounded overflow-auto">
         {JSON.stringify(data, null, 2)}
